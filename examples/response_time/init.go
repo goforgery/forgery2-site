d8c779478f19ba06f46acd9be378ba8e5012d68f
Ric Allinson <capecodehq@yahoo.co.uk>
2013-11-20 07:59:17 +0800

    Added sleep to response time example

--- examples/response_time/init.go
+++ examples/response_time/init.go
@@ -2,12 +2,14 @@ package main
 
 import (
     "github.com/ricallinson/forgery"
+    "time"
 )
 
 func main() {
     app := f.CreateServer()
     app.Use(f.ResponseTime())
     app.Get("/", func(req *f.Request, res *f.Response, next func()) {
+        time.Sleep(100 * time.Millisecond)
         res.Send("Response Time")
     })
     app.Listen(3000)
